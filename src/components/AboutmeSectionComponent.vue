eca8d8b38d50276dcd812cbc47a4da1f50303ea4
MaiLear <mairotavista@gmail.com>
2024-09-05 09:54:24 -0500

    feat: i change one paragraph in about me section

--- src/components/AboutmeSectionComponent.vue
+++ src/components/AboutmeSectionComponent.vue
@@ -1,42 +1,38 @@
 <template>
-<div class="d-flex">
-        <div>
-          <p>
-            Me llamo Maicol Andres, las personas a mi alrededor me describen
-            como una persona carismatica. Hace poco empeze en la programación
-            por lo que no cuento con mucha experiencia,
-            <span class="text-danger"
-              >pero si cuento con muchas ganas de aprender y utilizar mis
-              habilidades en la creacion de proyectos exitosos</span
-            >
-          </p>
+  <div class="d-flex">
+    <div>
+      <p>
+        Me llamo Maicol Andres, las personas a mi alrededor me describen como
+        una persona carismatica.
+      </p>
+      <p>
+        En mi experiencia tengo logros en
+        <span class="text-danger"
+          >mejorar el tiempo de carga de una consulta en el entorno de
+          pruebas.</span
+        >
+        Tambien colabore en la modularizacion de un proyecto mejorando su
+        escalabilidad
+      </p>
 
-          <p>
-            En mi poca experiencia tengo logros en
-            <span class="text-danger"
-              >mejorar el tiempo de carga de una consulta en el entorno de
-              pruebas.</span
-            >
-            Tambien colabore en la modularizacion de un proyecto mejorando su
-            escalabilidad
-          </p>
-
-          <p>
-            Desarrollando proyectos personales he podido practicar las
-            <span class="text-danger"
-              >buenas practicas en el desarrollo de software</span
-            >
-            y he podido adquirir conocimientos en varias tecnologias utilizadas
-            por multiples empresas para la construccion de sus sitios web
-          </p>
-        </div>
-        <img class="w-25 align-self-start img-thumbnail ms-4" style="transform: rotate(6deg);" src="../../public/img/aboutme.webp" alt="imagen sobre mi">
-      </div>
+      <p>
+        Desarrollando proyectos personales he podido practicar las
+        <span class="text-danger"
+          >buenas practicas en el desarrollo de software</span
+        >
+        y he podido adquirir conocimientos en varias tecnologias utilizadas por
+        multiples empresas para la construccion de sus sitios web
+      </p>
+    </div>
+    <img
+      class="w-25 align-self-start img-thumbnail ms-4"
+      style="transform: rotate(6deg)"
+      src="../../public/img/aboutme.webp"
+      alt="imagen sobre mi"
+    />
+  </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
 
-
-<style>
-</style>
+<style></style>
